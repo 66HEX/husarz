50b3b971923c12686c53403b2e240729ed95c0eb
66HEX <hextheprod@gmail.com>
2025-01-25 23:24:26 +0100

    chore: exclude libs folder from ESLint checks

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,9 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    ignorePatterns: ["app/libs/**"]
+  }
 ];
 
 export default eslintConfig;
