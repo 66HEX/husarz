4e312bee85e36a71301c7cff8d95af02cf68f1bd
66HEX <hextheprod@gmail.com>
2025-02-23 17:18:42 +0100

    hotfix

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -12,8 +12,7 @@ const compat = new FlatCompat({
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
   {
-    files: ["**/*.{js,jsx,ts,tsx}"],
-    ignores: ["app/libs/**"]  // Zmiana z ignorePatterns na ignores
+    ignorePatterns: ["app/libs/**"]
   }
 ];
 
